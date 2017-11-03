e6396838aa73fbceb610a0ffde7a9a199cbfb896
Panruo Wu <pwu11@icl.utk.edu>
2017-11-03 00:48:55 -0400

    factored bcast out; implemente pipeline

--- slate_Matrix.hh
+++ slate_Matrix.hh
@@ -116,6 +116,8 @@ public:
 
     void mm_summa(Matrix &a, Matrix &b, double alpha, double beta);
     void mm_summa_nb(Matrix &a, Matrix &b, double alpha, double beta);
+    void mm_summa_pl(Matrix &a, Matrix &b, double alpha, double beta, int la);
+    void mm_bcast(Matrix &a, Matrix &b, int M, int N,  int k);
     Memory *memory_;
 private:
     // TODO: replace by unordered_map
